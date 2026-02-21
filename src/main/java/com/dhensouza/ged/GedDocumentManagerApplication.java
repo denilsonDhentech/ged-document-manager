b01001d3ab1f573ff389307b7316f4da9441bf02
denilsonDhentech <denilson_contato@outlook.com>
2026-02-21 15:04:36 -0300

    chore: initial project structure from spring initializr

---
+++ src/main/java/com/dhensouza/ged/GedDocumentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dhensouza.ged;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GedDocumentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GedDocumentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhensouza/ged/GedDocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhensouza.ged;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GedDocumentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
