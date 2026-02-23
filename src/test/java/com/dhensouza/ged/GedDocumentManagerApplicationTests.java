d41f66ac6fad1a233fb3e41fb0d380ea3625440c
denilsonDhentech <denilson_contato@outlook.com>
2026-02-23 14:24:33 -0300

    feat: implement Account and Document services with CI/CD stability fixes

--- src/test/java/com/dhensouza/ged/GedDocumentManagerApplicationTests.java
+++ src/test/java/com/dhensouza/ged/GedDocumentManagerApplicationTests.java
@@ -1,9 +1,12 @@
 package com.dhensouza.ged;
 
+import com.dhensouza.ged.infrastructure.configuration.TestBeanConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestBeanConfiguration.class)
 class GedDocumentManagerApplicationTests {
 
 	@Test
