b724dc08ec7b80b44a081874654c1d30c062e611
denilsonDhentech <denilson_contato@outlook.com>
2026-02-23 19:29:25 -0300

    fix: extend JpaRepository in all repositories and fix test context loading

--- src/main/java/com/dhensouza/ged/domain/repository/DocumentVersionRepository.java
+++ src/main/java/com/dhensouza/ged/domain/repository/DocumentVersionRepository.java
@@ -1,11 +1,13 @@
 package com.dhensouza.ged.domain.repository;
 
 import com.dhensouza.ged.domain.entity.DocumentVersion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface DocumentVersionRepository {
+public interface DocumentVersionRepository extends JpaRepository<DocumentVersion, UUID> {
     DocumentVersion save(DocumentVersion version);
     Optional<Integer> findMaxVersionByDocumentId(UUID documentId);
 }
