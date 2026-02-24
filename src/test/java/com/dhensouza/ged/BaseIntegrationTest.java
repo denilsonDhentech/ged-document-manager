8122fd3ed30ee035240655e42f107f25c4bdeada
denilsonDhentech <denilson_contato@outlook.com>
2026-02-24 18:44:51 -0300

    test: test build 2

--- src/test/java/com/dhensouza/ged/BaseIntegrationTest.java
+++ src/test/java/com/dhensouza/ged/BaseIntegrationTest.java
@@ -2,16 +2,26 @@ package com.dhensouza.ged;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import com.dhensouza.ged.infrastructure.storage.S3StorageService;
 import tools.jackson.databind.ObjectMapper;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "storage.s3.endpoint=http://localhost:9000",
+        "storage.s3.access-key=mock",
+        "storage.s3.secret-key=mock",
+        "storage.s3.region=us-east-1",
+        "storage.s3.bucket-name=mock-bucket"
+})
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Sql(scripts = "/cleanup.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
@@ -24,8 +34,20 @@ public abstract class BaseIntegrationTest {
     protected ObjectMapper objectMapper;
 
     @MockitoBean
+    private S3StorageService storageService;
+
+    @MockitoBean
     private S3Client s3Client;
 
     @MockitoBean
     private S3Presigner s3Presigner;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @Primary
+        public ObjectMapper objectMapper() {
+            return new ObjectMapper();
+        }
+    }
 }
