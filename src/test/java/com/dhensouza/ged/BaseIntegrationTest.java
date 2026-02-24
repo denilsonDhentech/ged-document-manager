ac20ddc66e241704339f8696cfeaec5ebd4d1cc1
denilsonDhentech <denilson_contato@outlook.com>
2026-02-24 18:56:07 -0300

    test: test build 3

--- src/test/java/com/dhensouza/ged/BaseIntegrationTest.java
+++ src/test/java/com/dhensouza/ged/BaseIntegrationTest.java
@@ -1,18 +1,15 @@
 package com.dhensouza.ged;
 
+import com.dhensouza.ged.infrastructure.storage.S3StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import com.dhensouza.ged.infrastructure.storage.S3StorageService;
 import tools.jackson.databind.ObjectMapper;
 
 @SpringBootTest(properties = {
@@ -30,24 +27,14 @@ public abstract class BaseIntegrationTest {
     @Autowired
     protected MockMvc mockMvc;
 
-    @Autowired
-    protected ObjectMapper objectMapper;
+    protected ObjectMapper objectMapper = new ObjectMapper();
 
     @MockitoBean
     private S3StorageService storageService;
 
     @MockitoBean
-    private S3Client s3Client;
+    protected S3Client s3Client;
 
     @MockitoBean
-    private S3Presigner s3Presigner;
-
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        @Primary
-        public ObjectMapper objectMapper() {
-            return new ObjectMapper();
-        }
-    }
+    protected S3Presigner s3Presigner;
 }
